458570fb3e12d9afeec99de16e353f7dbce65a76
ZigCat <zigcat517@gmail.com>
2024-04-26 16:00:23 +0500

    develop: styles for dark theme

--- app/src/main/java/com/github/zigcat/blogplatform/api/ServerIP.java
+++ app/src/main/java/com/github/zigcat/blogplatform/api/ServerIP.java
@@ -3,7 +3,7 @@ package com.github.zigcat.blogplatform.api;
 public class ServerIP {
     public static String getCurrentIP(){
         return "192.168.68.101";
-        //return "192.168.225.106";
+        //return "172.20.10.3";
         //return "10.0.2.2";
     }
 }
